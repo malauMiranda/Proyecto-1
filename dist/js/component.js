fcd00a3e14442218dd7b546cc6c4c3fd638c04ac
malauMiranda <malaumiranda6@gmail.com>
2023-05-21 19:24:04 -0600

    Commit 4 (segunda entrega)
    Conecté el api a una parte del proyecto, se ven las diez recetas más vistas solo en las cards, me falta llenar alguna información, también arreglé un poco el código html para que las cards respondan mejor al responsive

--- dist/js/component.js
+++ dist/js/component.js
@@ -1,5 +1,46 @@
 
 const app = Vue.createApp({
+    data(){
+        return {
+            recipes: [],
 
+    }},
+
+    mounted: function () {
+        this.allRecipes = this.recipes;
+
+        axios({
+            method: 'get',
+            url: 'https://api.spoonacular.com/recipes/complexSearch?type=main course&apiKey=94c0451559b94743a024799352374d29'
+        })
+
+            .then(
+                (response) => {
+
+                    let items =response.data.results;
+                    console.log(items);
+
+                    this.recipes = [];
+
+                    items.forEach(element => {
+                        this.recipes.push({
+                            id: element.id,
+                            name: element.title,
+                            image: element.image,
+                            level:"Easy",
+                            category: "deafault category",
+                            servings: "6",
+                            likes:96,
+
+                        });
+                    });
+
+                }
+            )
+            .catch(
+                error => console.log(error)
+            );
+
+    },
 
 })
